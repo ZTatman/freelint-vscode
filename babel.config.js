e6d19bd34c2544c86a2fd6a9ac337d098b766078
ZTatman <tatmanzachariahit@gmail.com>
2025-04-17 15:13:50 -0400

    chore: format root level files with prettier as well

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-react",
+      '@babel/preset-react',
       {
-        runtime: "automatic",
+        runtime: 'automatic',
       },
     ],
   ],
